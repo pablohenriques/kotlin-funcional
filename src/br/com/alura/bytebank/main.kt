8d5a87eddb0960a052434b1a7c2b75c8815ddd72
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-11-10 23:36:35 -0300

    Lambdas e func anonimas com parametros

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,31 +1,33 @@
 package br.com.alura.bytebank
 
 fun main() {
-    val minhaFuncaoLambda = {
-        println("Executa como Lambda")
-    }
-    println(minhaFuncaoLambda)
-
-    val minhaFuncaoAnonima: () -> Unit = fun() {
-        println("Executa como anonima")
-    }
-    println(minhaFuncaoAnonima)
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+//    val minhaFuncaoLambda = {
+//        println("Executa como Lambda")
+//    }
+//    println(minhaFuncaoLambda)
+//
+//    val minhaFuncaoAnonima: () -> Unit = fun() {
+//        println("Executa como anonima")
+//    }
+//    println(minhaFuncaoAnonima)
 }
 
 private fun testaTipoFuncaoClasse() {
-    val cm: () -> Unit = CMensagem()
+    val cs: (Int, Int) -> Int = Soma()
+    println(cs(10, 15))
 }
 
 private fun testaTipoFuncaoReferencia() {
-    val m: () -> Unit = ::mensagem
+    val s: (Int, Int) -> Int = ::soma
+    println(s(10, 10))
 }
 
-fun mensagem() {
-    println("Funcao Mensagem")
+fun soma(a: Int, b: Int): Int {
+    return a + b
 }
 
-class CMensagem : () -> Unit {
-    override fun invoke() {
-        println("Classe de Mensagem")
-    }
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
 }
