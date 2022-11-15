f0f2e172434a9018216ef0835b208e1293583d51
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-11-15 19:36:41 -0300

    Implementando o HOF higher-order-function

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,6 +1,8 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
 //    val endereco = Endereco(logradouro = "rua verg", numero = 3185)
@@ -17,8 +19,23 @@ fun main() {
         Endereco(),
         Endereco(complemento = "apartamento")
     )
-        .filter { endereco ->
-            endereco.complemento.isNotEmpty()
-        }
-        .let (::println)
+        .filter ( predicate= {endereco -> endereco.complemento.isNotEmpty() } )
+        .let (block = (::println))
+
+    soma(1, 5, resultado = (::println))
+
+    val autenticavel = object : Autenticavel {
+        val senha = 1234
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    SistemaInterno().entra(autenticavel, 1234, autenticado = {
+        println("realizar pagamento")
+    })
+}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    println("Antes da Soma")
+    resultado(a + b)
+    println("Depois da Soma")
 }
