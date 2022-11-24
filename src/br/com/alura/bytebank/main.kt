3aa83a6dc308518f1cb32c8fe9976f295e278ceb
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-11-23 23:02:23 -0300

    Utilizando with

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,41 +1,21 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
-//    val endereco = Endereco(logradouro = "rua verg", numero = 3185)
-//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-//    println(enderecoEmMaiusculo)
-
-    Endereco(logradouro = "rua verg", numero = 3185)
-        .run {
-            "$logradouro, $numero".toUpperCase()
-        }.let { enderecoEmMaiusculo -> println(enderecoEmMaiusculo) }
-
-    listOf(
-        Endereco(complemento = "casa"),
-        Endereco(),
-        Endereco(complemento = "apartamento")
-    )
-        .filter ( predicate= {endereco -> endereco.complemento.isNotEmpty() } )
-        .let (block = (::println))
-
-    soma(1, 5, resultado = (::println))
-
-    val autenticavel = object : Autenticavel {
-        val senha = 1234
-        override fun autentica(senha: Int) = this.senha == senha
-    }
-
-    SistemaInterno().entra(autenticavel, 1234, autenticado = {
-        println("realizar pagamento")
-    })
+    testaWith()
 }
 
-fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
-    println("Antes da Soma")
-    resultado(a + b)
-    println("Depois da Soma")
+fun testaWith() {
+    with(Endereco()) {
+        logradouro = "rua vergueiro"
+        numero = 3128
+        bairro = "Vila Mariana"
+        estado = "SP"
+        cep = "02310-063"
+        complemento = "Apartamento"
+        completo()
+    }.let { endereco: String ->
+        println(endereco)
+    }
 }
